ae0bf8d6d2fc276108c11edfc6b3719168ec07a9
az13js <1654602334@qq.com>
2020-04-02 22:27:31 +0800

    预测4/3

--- build.php
+++ build.php
@@ -4,9 +4,9 @@ include 'autoload.php';
 
 class Config {
     public static $trainDataStartId = 2112;
-    public static $trainDataEndId = 6363;
-    public static $testDataStartId = 6333;
-    public static $testDataEndId = 6363;
+    public static $trainDataEndId = 6364;
+    public static $testDataStartId = 6334;
+    public static $testDataEndId = 6364;
     public static $data = [];
     public static $index = 0;
     public static $inputDay = 30;
